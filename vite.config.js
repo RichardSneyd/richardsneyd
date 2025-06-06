5cacc81a21c61fc7e7f62191ea2a309d780f2cd6
RichardSneyd <richardsneyd@hotmail.com>
2025-06-06 11:00:27 +0100

    file paths fixed for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './',
-  base: '/richardsneyd/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
